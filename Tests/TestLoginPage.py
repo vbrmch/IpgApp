4d33cff5b69ad4d7074db4ed6258e8f1a4b98939
vadim <>
2023-02-07 00:14:03 +0100

    del file

--- Tests/TestLoginPage.py
+++ Tests/TestLoginPage.py
@@ -14,4 +14,4 @@ class TestLoginPage:
         login_page = LoginPage(page)
         login_page.visit()
         login_page.login(email='smit71257@gmail.com', password='1111')
-        time.sleep(2)
+        time.sleep(23)
